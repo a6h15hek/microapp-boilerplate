f5790aefa05e81704e6bd3f6b72a2be62bd5cbb4
a6h15hek <a6h15hek@outlook.com>
2024-09-25 12:24:53 +0530

    not needed import removed

--- views/components/HelloWorld.js
+++ views/components/HelloWorld.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useAuthentication } from '@views/AuthenticationContext';
 
 function HelloWorld() {
-  const client = useAuthentication();
-  return client.isAuthenticated ? (<div>Hello World!!</div>) : '';
+  return <div>Hello World!!</div>;
 }
 
 export default HelloWorld;
