97a2d4b3b63f637eb4885224a7a521ff8dadda9a
Abhishek M. Yadav <a6h15hek@outlook.com>
2024-09-26 21:52:10 +0530

    not needed things removed

--- views/index.js
+++ views/index.js
@@ -13,7 +13,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MainApp />
-    {process.env.PORT}
-    {process.env.ENV}
   </React.StrictMode>
 );
